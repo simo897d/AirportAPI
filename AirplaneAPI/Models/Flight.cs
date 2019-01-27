f97f52fa1e8004490319cdf6ab15f69aac7c3af1
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2019-01-27 13:39:20 +0100

    ihji

---
+++ AirplaneAPI/Models/Flight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirplaneAPI.Models {
+    public class Flight {
+        public int FlightId { get; set; }
+        public string AirCraftType { get; set; }
+        public string FromLocation { get; set; }
+        public string ToLocation { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+}
